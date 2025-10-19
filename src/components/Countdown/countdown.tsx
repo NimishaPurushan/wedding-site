3c361270bf5b8e5d75d883d1cc9d0a63925868af
nimisha <nimisha.mec@gmail.com>
2025-10-19 13:07:59 -0400

    updated reception address

--- src/components/Countdown/countdown.tsx
+++ src/components/Countdown/countdown.tsx
@@ -33,21 +33,21 @@ export const Countdown = () => {
       <div className="flex gap-4 text-5xl font-bold text-white bg-orange-300 px-6 py-4 rounded-xl shadow-md">
         <div className="flex flex-col items-center">
           <span>{pad(timeLeft.days)}</span>
-          <span className="text-sm font-medium text-gray-300 mt-1">Days</span>
+          <span className="text-sm font-medium text-gray-400 mt-1">Days</span>
         </div>
         <div className="flex flex-col items-center">
           <span>{pad(timeLeft.hours)}</span>
-          <span className="text-sm font-medium text-gray-300 mt-1">Hours</span>
+          <span className="text-sm font-medium text-gray-400 mt-1">Hours</span>
         </div>
         <div className="flex flex-col items-center">
           <span>{pad(timeLeft.minutes)}</span>
-          <span className="text-sm font-medium text-gray-300 mt-1">
+          <span className="text-sm font-medium text-gray-400 mt-1">
             Minutes
           </span>
         </div>
         <div className="flex flex-col items-center">
           <span>{pad(timeLeft.seconds)}</span>
-          <span className="text-sm font-medium text-gray-300 mt-1">
+          <span className="text-sm font-medium text-gray-400 mt-1">
             Seconds
           </span>
         </div>
